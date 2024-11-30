9c5c65aa31f436bc0639ce5bcc96e26181afba52
tomason182 <elotro182@hotmail.com>
2024-11-30 11:33:54 -0300

    add: First commit. Folder structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Home />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
