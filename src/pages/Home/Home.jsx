739d3a6a8afb3de95b017b45eb454a653af57ec0
tomason182 <elotro182@hotmail.com>
2024-11-30 18:17:28 -0300

    add: about us and rooms containers

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,6 +10,25 @@ export default function Home() {
       </section>
       <section className={styles.about}>
         <h2>Acerca de nuestra casa</h2>
+        <p>
+          En La Casa del Viajero encontraras un entorno cuidado con el cariño de
+          quienes aprecian la naturaleza podrás encontrar casas construidas en
+          adobe y madera, que las hacen frescas en el verano y cálidas cuando
+          llegan los tiempos más fríos, armonizando con el calor humano que
+          compartimos en este rincón de la Patagonia cordillerana.
+        </p>
+        <button>Mas Info</button>
+      </section>
+      <section className={styles.rooms}>
+        <div className={styles.backgroundImg}>
+          <h2>Nuestros cuartos</h2>
+        </div>
+
+        <div>
+          <div>El refugio</div>
+          <div>La casita</div>
+          <div>El ranchito</div>
+        </div>
       </section>
     </>
   );
