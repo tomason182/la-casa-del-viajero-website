5a63b93fa2971910b4bd3d38db0d8095ab579ca6
tomason182 <elotro182@hotmail.com>
2024-11-30 12:58:46 -0300

    add: display header elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import Home from "./pages/Home/Home.jsx";
+import "./App.css";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <main>
         <Home />
       </main>
       <Footer />
-    </div>
+    </>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,19 @@
+import styles from "./Header.module.css";
+
 export default function Header() {
   return (
-    <header>
-      <h1>This is the header of the page</h1>
+    <header className={styles.header}>
+      <div>
+        <h1>Logo del hostel</h1>
+      </div>
+      <nav>
+        <ul className={styles.navList}>
+          <li>Servicios</li>
+          <li>Nuestros cuartos</li>
+          <li>Contactanos</li>
+          <li>Sobre Nosotros</li>
+        </ul>
+      </nav>
     </header>
   );
 }
