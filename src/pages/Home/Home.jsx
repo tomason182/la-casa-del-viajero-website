8e5b490cc8015fd3cfae73c6b14bef0cb424c252
tomason182 <elotro182@hotmail.com>
2024-11-30 17:17:50 -0300

    fix: position bookingContent absolute

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,10 +2,15 @@ import styles from "./Home.module.css";
 
 export default function Home() {
   return (
-    <section className={styles.mainContent}>
-      <div className={styles.bookingContent}>
-        <p>Reservations</p>
-      </div>
-    </section>
+    <>
+      <section className={styles.mainContent}>
+        <div className={styles.bookingContent}>
+          <h2>Reservá tu próxima estadia</h2>
+        </div>
+      </section>
+      <section className={styles.about}>
+        <h2>Acerca de nuestra casa</h2>
+      </section>
+    </>
   );
 }
