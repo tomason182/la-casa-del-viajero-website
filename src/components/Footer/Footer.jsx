4b70e45b313bc59e10859c69568dec1f53f98312
tomason182 <elotro182@hotmail.com>
2024-12-02 19:13:57 -0300

    add: footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,28 @@
+import styles from "./Footer.module.css";
+
 export default function Footer() {
   return (
-    <footer>
-      <h1>This is the footer of the page</h1>
+    <footer className={styles.footer}>
+      <div className={styles.content}>
+        <div>
+          <h2>La Casa del Viajero</h2>
+        </div>
+        <div>
+          <p>Av. Las Flores 4220</p>
+          <p>El bolsón, Rio Negro</p>
+        </div>
+        <div>
+          <p>Tel: 294 - xxxx - xxxx</p>
+          <p>email: some@email.com</p>
+        </div>
+        <div>
+          <p>facebook</p>
+          <p>instagram</p>
+        </div>
+      </div>
+      <div className={styles.copyright}>
+        <p>© SimpleHostel - www.simplehostel.net</p>
+      </div>
     </footer>
   );
 }
