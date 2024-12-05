11855c5b170fb3f30338f53e54412c8f7006239d
tomason182 <elotro182@hotmail.com>
2024-12-05 17:36:59 -0300

    add: Search results component

---
+++ src/pages/SearchResult/SearchResult.jsx
@@ -0,0 +1,52 @@
+import Header from "../../components/Header/Header.jsx";
+import AvailabilityForm from "../../components/Forms/AvailabilityForm";
+import styles from "./SearchResult.module.css";
+
+export default function SearchResult() {
+  const propertyInfo = {
+    name: "La Casa del Viajero",
+    address: "Av. Las Flores 4220",
+    province: "Rio Negro",
+    country: "Argentina",
+  };
+
+  return (
+    <>
+      <Header />
+      <main>
+        <section className={styles.searchContainer}>
+          <AvailabilityForm />
+        </section>
+        <section className={styles.searchDisplay}>
+          <div className={styles.propertyInfo}>
+            <h1>{propertyInfo.name}</h1>
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#000000"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <circle cx="12" cy="10" r="3" />
+                <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+              </svg>
+              <p>
+                {propertyInfo.address}, {propertyInfo.province},
+                {propertyInfo.country}
+              </p>
+            </div>
+            <div className={styles.mainContent}>
+              <div className={styles.roomsContainer}></div>
+              <div className={styles.priceDetailsContainer}></div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
